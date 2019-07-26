b409dd07c5abfcc3ef6770dfb0edd7c40e16c8f0
dv16sen <dv16sen@cs.umu.se>
2019-07-26 19:42:04 +0200

    Added missing import statement to script folder.

--- create-npm-module.sh
+++ create-npm-module.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+SCRIPT_FOLDER=$(dirname $(readlink -f $0))
+. "$SCRIPT_FOLDER/lib/create-app-script.sh"
 
 startCreateScript "create-npm-module" "$1"
 includeRepository "npm-module-template"
